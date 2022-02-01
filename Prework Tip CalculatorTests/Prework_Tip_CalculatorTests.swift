0d3700d687d344fd6bac8c6a10b94421c6f3182e
Matt-8A <moimportant.important@gmail.com>
2022-01-31 21:17:43 -0800

    Added prework files to repository

---
+++ Prework Tip CalculatorTests/Prework_Tip_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Prework_Tip_CalculatorTests.swift
+//  Prework Tip CalculatorTests
+//
+//  Created by Matthew Ochoa on 1/31/22.
+//  Copyright © 2022 Matthew Ochoa. All rights reserved.
+//
+
+import XCTest
+@testable import Prework_Tip_Calculator
+
+class Prework_Tip_CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
